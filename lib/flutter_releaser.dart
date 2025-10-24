671f27394b6ac575dfd6f2af3d19b0ffc56685ed
Hasan Demirtaş <utsukushihito@outlook.com>
2025-10-24 17:39:03 +0300

    const.

--- lib/flutter_releaser.dart
+++ lib/flutter_releaser.dart
@@ -1,6 +1,8 @@
 import "package:flutter_releaser/flutter_releaser_platform_interface.dart";
 
 class FlutterReleaser {
+  const FlutterReleaser();
+
   Future<String?> getPlatformVersion() =>
       FlutterReleaserPlatform.instance.getPlatformVersion();
 }
--- lib/flutter_releaser_method_channel.dart
+++ lib/flutter_releaser_method_channel.dart
@@ -3,9 +3,7 @@ import "package:flutter/services.dart";
 
 import "package:flutter_releaser/flutter_releaser_platform_interface.dart";
 
-/// An implementation of [FlutterReleaserPlatform] that uses method channels.
 class MethodChannelFlutterReleaser extends FlutterReleaserPlatform {
-  /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel("flutter_releaser");
 
