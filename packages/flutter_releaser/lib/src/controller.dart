11e0e29bcdf0fd3d9b737e2c8f95971bf3b735b7
Hasan Demirtaş <utsukushihito@outlook.com>
2025-10-25 17:41:44 +0300

    simplify.

--- packages/flutter_releaser/lib/src/controller.dart
+++ packages/flutter_releaser/lib/src/controller.dart
@@ -2,62 +2,33 @@ import "dart:async";
 
 import "package:flutter/foundation.dart";
 import "package:flutter_releaser/flutter_releaser.dart";
+import "package:flutter_releaser/src/download.dart";
 import "package:flutter_releaser/src/models.dart";
 import "package:flutter_releaser/src/progress.dart";
 import "package:flutter_releaser/src/version.dart";
 
-class UpdateController extends ChangeNotifier {
+class UpdateController {
   final FlutterReleaserSettings settings;
 
-  Version? get nextVersion => _nextVersion;
-  Version? _nextVersion;
-
-  DownloadProgress? get downloadProgress => _downloadProgress;
-  DownloadProgress? _downloadProgress;
-
-  StreamSubscription<DownloadProgress>? _subscription;
+  ValueNotifier<Version?> nextVersionNotifier = ValueNotifier(null);
+  ValueNotifier<DownloadProgress?> downloadProgressNotifier = ValueNotifier(
+    null,
+  );
 
   UpdateController({required this.settings});
 
-  @override
-  Future<void> dispose() async {
-    super.dispose();
-
-    await _subscription?.cancel();
-  }
-
-  Future<Version?> check() => retrieveNewVersion(settings);
-
-  Future<Version?> checkAndNotify() async {
-    final newVersion = await check();
-
-    if (newVersion != null) {
-      _nextVersion = newVersion;
-      notifyListeners();
-    } else if (_nextVersion != null) {
-      _nextVersion = null;
-      notifyListeners();
-    }
-
+  Future<Version?> check() async {
+    final newVersion = await retrieveNewVersion(settings);
+    nextVersionNotifier.value = newVersion;
     return newVersion;
   }
 
-  Future<Stream<DownloadProgress>> download() async => const Stream.empty();
+  Future<void> download() async {
+    final nextVersion = nextVersionNotifier.value;
+    if (nextVersion == null) {
+      throw Exception("Could not found next version, please use #check");
+    }
 
-  Future<void> downloadAndNotify() async {
-    await _subscription?.cancel();
-
-    final stream = await download();
-    _subscription = stream.listen(
-      (event) {
-        _downloadProgress = event;
-        notifyListeners();
-      },
-      onDone: () {
-        _downloadProgress = null;
-        notifyListeners();
-      },
-      cancelOnError: true,
-    );
+    return downloadVersion(settings, nextVersion, downloadProgressNotifier);
   }
 }
---
+++ packages/flutter_releaser/lib/src/download.dart
@@ -0,0 +1,12 @@
+import "dart:async";
+
+import "package:flutter/foundation.dart";
+import "package:flutter_releaser/flutter_releaser.dart";
+import "package:flutter_releaser/src/models.dart";
+import "package:flutter_releaser/src/progress.dart";
+
+Future<void> downloadVersion(
+  FlutterReleaserSettings settings,
+  Version version,
+  ValueNotifier<DownloadProgress?> downloadProgressNotifier,
+) async {}
