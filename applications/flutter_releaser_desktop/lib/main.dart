6a56e2cb1cbea3b4c1d45fb563ced5ca8c17f7a1
Hasan Demirtaş <utsukushihito@outlook.com>
2025-10-29 17:47:44 +0300

    chore: format.

--- applications/flutter_releaser_desktop/lib/main.dart
+++ applications/flutter_releaser_desktop/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() {
   runApp(const MyApp());
@@ -8,9 +8,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-    );
-  }
+  Widget build(BuildContext context) =>
+      const MaterialApp(title: "Flutter Demo");
 }
