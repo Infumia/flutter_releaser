0c16cac20eec738247bbd53712977cdb565a88e7
Hasan Demirtaş <utsukushihito@outlook.com>
2025-10-30 16:04:01 +0300

    feat: flutter_releaser_desktop show version using package info plus.

--- applications/flutter_releaser_desktop/lib/main.dart
+++ applications/flutter_releaser_desktop/lib/main.dart
@@ -1,13 +1,27 @@
+import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
+import "package:package_info_plus/package_info_plus.dart";
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  final info = await PackageInfo.fromPlatform();
+  runApp(MyApp(info: info));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final PackageInfo info;
+
+  const MyApp({required this.info, super.key});
 
   @override
-  Widget build(BuildContext context) =>
-      const MaterialApp(title: "Flutter Demo");
+  Widget build(BuildContext context) => MaterialApp(
+    title: "Flutter Releaser",
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(body: Text("$info")),
+  );
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<PackageInfo>("info", info));
+  }
 }
