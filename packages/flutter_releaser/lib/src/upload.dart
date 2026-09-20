9688e65b1c64d8d5aae0220f644bfcdb862c765a
Hasan Demirtaş <utsukushihito@outlook.com>
2026-09-20 09:45:05 +0300

    Update upload.dart

--- packages/flutter_releaser/lib/src/upload.dart
+++ packages/flutter_releaser/lib/src/upload.dart
@@ -1,16 +1,43 @@
 import "package:flutter_releaser/flutter_releaser.dart";
 import "package:flutter_releaser/src/s3/upload.dart";
 
-Future<void> uploadVersion(
+Future<int> uploadVersion(
   FlutterReleaserSettings settings,
   UploadVersionRequest request,
   Ref<UploadProgress?> uploadProgressRef,
 ) async {
-  final file = switch (settings.fileStorage) {
+  final versionId = await switch (settings.fileStorage) {
     FileStorage.s3 => uploadS3File(settings, request, uploadProgressRef),
   };
 
   uploadProgressRef.value = uploadProgressRef.value?.copyWith(finished: true);
 
-  return file;
+  return versionId;
+}
+
+Future<void> uploadInstallerFile(
+  FlutterReleaserSettings settings,
+  int versionId,
+  String installerPath,
+  Ref<UploadProgress?> uploadProgressRef,
+) async {
+  await switch (settings.fileStorage) {
+    FileStorage.s3 => uploadS3Installer(
+      settings,
+      versionId,
+      installerPath,
+      uploadProgressRef,
+    ),
+  };
+
+  uploadProgressRef.value = uploadProgressRef.value?.copyWith(finished: true);
+}
+
+Future<void> confirmVersion(
+  FlutterReleaserSettings settings,
+  int versionId,
+) async {
+  await switch (settings.fileStorage) {
+    FileStorage.s3 => confirmS3Upload(settings, versionId),
+  };
 }
